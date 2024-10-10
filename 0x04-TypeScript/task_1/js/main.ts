f2242ef52e6f243ab35bb75f4511f8e98e860136
omotomiwa26 <omotomiwa26@gmail.com>
2024-10-10 09:17:05 +0100

    upadated main file for task 2 and 3

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -13,6 +13,29 @@ const teacher3: Teacher = {
     lastName: 'Doe',
     location: 'London',
     contract: false,
-  };
-  
-  console.log(teacher3);
+};
+
+console.log(teacher3);
+
+//Task 2. Extending the Teacher class
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+console.log(director1);
+
+//Task 3. Printing teachers
+
+function printTeacher(firstName: string, lastName: string): string {
+    return `${firstName[0]} ${lastName}`;
+}
+
+printTeacher("John", "Doe")
